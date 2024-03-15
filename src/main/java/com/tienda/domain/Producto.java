aae19b657215f7ada2efccb0910d8dcca2edccce
RonaldSolano2907 <rsolanovac@gmail.com>
2024-03-14 21:00:50 -0600

    Practica #4
    Elaboracion de Practica#4

---
+++ src/main/java/com/tienda/domain/Producto.java
@@ -0,0 +1,42 @@
+package com.tienda.domain;
+
+import jakarta.persistence.*;
+import java.io.Serializable;
+import java.util.List;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "producto")
+
+public class Producto implements Serializable { //serializacion porque se va almacenar ciertos datos en el disco
+
+    private static final long serialVersionUID = 1L; //para poder hacer el ciclo de la sumatoria de la producto.
+
+    @Id //id producto es la llave de la tabla producto. 
+    @GeneratedValue(strategy = GenerationType.IDENTITY) //Los valores generados que estrategia usan, identico a la BD 
+    @Column(name = "id_producto") //decir cual es el nombre en la base de datos. Se hace la asociación 
+    private long idProducto;
+    //private long idCategoria;
+    private String descripcion;
+    private String detalle;
+    private double precio;
+    private int existencias;
+    private String rutaImagen;
+    private boolean activo;
+    
+    
+    @ManyToOne //categoria de muchos a uno
+    @JoinColumn (name="id_categoria") //esta es la union con la tabla de productos (foreign key)
+    Categoria categoria; //informacion ID
+    
+    public Producto() {
+    }
+
+    public Producto(String descripcion, boolean activo) {
+        this.descripcion = descripcion;
+        this.activo = activo;
+    }
+
+}
+        
